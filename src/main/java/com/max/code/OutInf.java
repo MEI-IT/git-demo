e216c978f4e1cf1134a803f5d1711c19c60b2836
Max <2498138592@qq.com>
2022-12-09 21:28:43 +0800

    one hot-fix

--- src/main/java/com/max/code/OutInf.java
+++ src/main/java/com/max/code/OutInf.java
@@ -4,5 +4,7 @@ public class OutInf {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,git2");
+        System.out.println("hello,git3");
+
     }
 }
