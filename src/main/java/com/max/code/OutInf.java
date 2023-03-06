9b4313b1ee204ed11d9eff3457bbff2113007e9f
max <meiax@bijienetworks.com>
2023-03-06 12:21:51 +0800

    t11

--- src/main/java/com/max/code/OutInf.java
+++ src/main/java/com/max/code/OutInf.java
@@ -3,11 +3,6 @@ package com.max.code;
 public class OutInf {
     public static void main(String[] args) {
         System.out.println("hello,git");
-        System.out.println("hello,git2");
-        System.out.println("hello,git3");
-        System.out.println("master,commit");
-        System.out.println("hot-fix,commit");
-        System.out.println("pull-github");
-        System.out.println("pull-rename-test");
+       
     }
 }
